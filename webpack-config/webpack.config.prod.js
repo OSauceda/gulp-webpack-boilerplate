099515484a4ab5c461361ee3a87623e14fbccfb7
OSauceda <osfesa462@gmail.com>
2019-06-30 22:41:58 -0600

    added webpack configuration for production

--- webpack-config/webpack.config.prod.js
+++ webpack-config/webpack.config.prod.js
@@ -1,1 +1,33 @@
 // Webpack configuration for production environment
+const webpackMerge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const baseConfig = require('./webpack-config/webpack.config.base.js');
+
+const prodConfig = {
+  mode: 'production',
+  devtool: 'source-map',
+  performance: {
+    hints: false
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        sourceMap: true,
+        terserOptions: {
+          ecma: 5,
+          mangle: true,
+          beautify: false,
+          compress: {
+            drop_console: false
+          },
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  }
+}
+
+module.exports = webpackMerge(baseConfig, prodConfig)
