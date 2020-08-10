769392c70ef74edf672b084bfcfac36d1c71e5c1
OSauceda <osfesa462@gmail.com>
2020-08-10 11:02:56 -0600

    Updated gulp configuration, updated other dependencies.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,20 @@
 // Dependencies
-import gulp from 'gulp';
-import sourcemaps from 'gulp-sourcemaps';
+import gulp, { watch, series, parallel } from 'gulp';
+import notify from 'gulp-notify';
 
 // SCSS
 import sass from 'gulp-sass';
 import cssnano from 'gulp-cssnano';
+import cached from 'gulp-cached';
+import dependents from 'gulp-dependents';
+
+sass.compiler = require('node-sass');
 
 // Webpack
 import { scripts } from './webpack.config.js';
 
-// SCSS task
-gulp.task('scss', () => {
+// Compile SCSS to CSS
+export function styles() {
   const isProd = process.env.NODE_ENV === 'production';
   const sassOptions = {
     errLogToConsole: true,
@@ -18,7 +22,7 @@ gulp.task('scss', () => {
   };
   const defaults = {
     autoprefixer: {
-      browsers: ['last 2 version', 'ie >= 10'],
+      browsers: ['last 3 version', 'ie >= 11'],
       add: true
     },
     reduceIdents: false
@@ -34,21 +38,34 @@ gulp.task('scss', () => {
     svgo: false,
     minifyFontValues: false,
   };
-  let cssNanoOptions;
-
-  if (isProd) {
-    cssNanoOptions = Object.assign({}, defaults);
-  } else {
-    cssNanoOptions = Object.assign({}, defaults, devOptions);
-  }
+  const cssNanoOptions = (isProd) ? Object.assign({}, defaults) : Object.assign({}, defaults, devOptions);
 
   return gulp.src('./src/scss/**/*.scss')
-    .pipe(sourcemaps.init())
+    .pipe(cached('scss'))
+    .pipe(dependents())
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(cssnano(cssNanoOptions))
-    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./dist/assets/css/'));
-});
+}
 
-// Development task
-gulp.task('default', scripts);
+// Chokidar event listener callback
+const onFileChangeHandler = (task, message) => {
+
+  return (newPath) => {
+    console.log(`File ${newPath} was changed, running task '${task}'...`);
+    notify({
+      title: `Starting task '${task}'...`,
+      message,
+      sound: false
+    }).write('');
+  };
+};
+
+function watchTask() {
+
+  watch('./src/scss/**/*.scss', styles)
+    .on('change', onFileChangeHandler('scss', 'Proceeding to compile SCSS...'));
+}
+
+// default task (dev)
+exports.default = series([parallel([styles, scripts])], watchTask);
